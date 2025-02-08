b1408ac268264e51481ddbcdf0fdeedd72840c55
Giovani-DevCoder <DGSamuel255@Outlook.com>
2025-02-07 21:24:54 -0400

    cambios febrero 7

---
+++ Frontend/src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <div className='h-80 w-full bg-zinc-800'>
+            banner produc
+        </div>
+    </div>
+  )
+}
+
+export default Banner
--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductList from '../components/ProductList';
+import Banner from '../components/Banner';
 
 
 const Home = () => {
   return (
     <div>
         <ProductList />
+        <Banner />
     </div>
   )
 };
